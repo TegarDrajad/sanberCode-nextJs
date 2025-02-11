7ab392c77cb60c9cf54caf2f1de0dc62b70b3bfd
TegarDrajad <tegardsv@gmail.com>
2025-02-11 16:01:00 +0700

    Routing and Styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import RootLayout from '@/layout'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  // console.log('router ->', router)
+  const metaTitle = router.pathname === '/'? "Home" : router.pathname.replace('/', '')
+
+  return (
+    <RootLayout metaTitle={metaTitle}>
+      <Component {...pageProps}/>
+    </RootLayout>
+  )
 }
